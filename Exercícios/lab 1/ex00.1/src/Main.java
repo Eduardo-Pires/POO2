ae32fd770d6344bda766b18b5986f7456c4f634b
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-03-03 20:45:29 -0300

    tentativa falha

---
+++ Exercícios/lab 1/ex00.1/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ex01_StringComp.stringComp("cabelo", "cab"));
+    }
+}
---
+++ Exercícios/lab 1/ex00.1/src/ex01_StringComp.java
@@ -0,0 +1,7 @@
+public class ex01_StringComp
+{
+    public static Boolean stringComp(String string1, String string2)
+    {
+        for (int i)
+    }
+}
