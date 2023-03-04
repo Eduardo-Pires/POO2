5f24049d333ca1152eb2a2e2f05f35f71235ca11
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-03-04 09:00:35 -0300

    Update ex01_StringComp.java

--- Exercícios/lab 1/ex00.1/src/ex01_StringComp.java
+++ Exercícios/lab 1/ex00.1/src/ex01_StringComp.java
@@ -2,6 +2,21 @@ public class ex01_StringComp
 {
     public static Boolean stringComp(String string1, String string2)
     {
-        for (int i)
+        Boolean ok = true;
+
+        if (stringComp(string1, string2))
+        {
+            return ok;
+        }
+        else if (string1.length() > string2.length())
+        {
+            int i = string1.length(), j = string2.length();
+
+            while(i < 0 && j < 0)
+            {
+
+            }
+        }
+
     }
 }
