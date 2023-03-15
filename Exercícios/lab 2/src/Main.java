61754df99fcbd1980ea94ce2240e3bb291157144
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-03-15 19:01:15 -0300

    comecei mudanças 1

--- Exercícios/lab 2/src/Main.java
+++ Exercícios/lab 2/src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        //deixar genérico, percorrer um for e ler vários arquivos sendo todos em uma pasta ou pedir para o usuário a quantidade de arquivos e somar o total de linha de todos os arquivos
+        /*deixar genérico, percorrer um for e ler vários arquivos sendo todos em uma pasta ou pedir
+         *para o usuário a quantidade de arquivos e somar o total de linha de todos os arquivos
+         */
+        for (int i = 0; i < 3; )
+        (new ex01("src/file" + 1 +".txt")).start();
 
-        (new ex01("src/file1.txt")).start();
-        (new ex01("src/file2.txt")).start();
-        (new ex01("src/file3.txt")).start();
     }
 }
