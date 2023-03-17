8b1875608769d498058a7bcccae9cdccad05cdc0
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-03-17 16:14:23 -0300

    finalizado exercício 1 do lab 2

--- Exercícios/lab 2/src/Main.java
+++ Exercícios/lab 2/src/Main.java
@@ -1,10 +1,14 @@
+import java.io.File;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        /*deixar genérico, percorrer um for e ler vários arquivos sendo todos em uma pasta ou pedir
-         *para o usuário a quantidade de arquivos e somar o total de linha de todos os arquivos
-         */
-        for (int i = 0; i < 3; )
-        (new ex01("src/file" + 1 +".txt")).start();
+        File files= new File("src/files");
+        int filesSize = Objects.requireNonNull(files.list()).length;
 
+        for (int i = 1; i <= filesSize; i++)
+        {
+            (new ex01("src/files/file" + i +".txt")).start();
+        }
     }
 }
